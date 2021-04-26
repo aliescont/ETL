2f6d6b769690438396b126e9debd7f557922fca3
aliescont <aescontrela@gmail.com>
2021-04-26 23:59:29 +0200

    add tags

--- etl_bsoup.py
+++ etl_bsoup.py
@@ -4,8 +4,9 @@ import json
 import pandas as pd
 import re
 
-def extract_data(url):
+def extract_games(url):
     game_list = []
+    tag_list = []
    
     r = requests.get(url)
     data = r.json()
@@ -19,13 +20,29 @@ def extract_data(url):
         game_dict = {
             'link' : game['href'],
             'id' : link.group(2).replace('/', '') ,
-            'name' : link.group(3).replace('/', '')
-            
+            'name' : link.group(3).replace('/', ''),
+            #'review' : game.find('span', {'class': 'search_review_summary positive'}),
+            'tags': add_label(game['href'])
             }
        
         game_list.append(game_dict)
     
     return game_list
 
-data_extract = extract_data('https://store.steampowered.com/search/results/?query&start=0&count=100&dynamic_data=&sort_by=_ASC&snr=1_7_7_7000_7&filter=topsellers&tags=19&infinite=1')
-print(data_extract)
+def add_label(game_url):
+    tag_list = []
+    tag_r = requests.get(game_url)
+    tag_soup = BeautifulSoup(tag_r.content,'html.parser')
+
+    for label in tag_soup.find_all('a', {'class': 'app_tag'}):
+        tag = label.text
+        tag = re.sub('([\\r\\n\\t]+)', '', tag)
+        tag_list.append(tag)
+
+    return tag_list
+    
+
+
+data_extract = extract_games('https://store.steampowered.com/search/results/?query&start=0&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_7000_7&filter=topsellers&tags=19&infinite=1')
+
+print(data_extract[0])
