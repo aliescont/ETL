a4fa6be93230137bda5740fa4b1a4d14bbb3164a
aliescont <aescontrela@gmail.com>
2021-04-27 23:40:20 +0200

    include all games with infinite scroll

--- etl_bsoup.py
+++ etl_bsoup.py
@@ -40,9 +40,23 @@ def add_label(game_url):
         tag_list.append(tag)
 
     return tag_list
+     
+
+
+
+if __name__ == "__main__":
+    game_data = []
+
+    url = 'https://store.steampowered.com/search/results/?query&start=0&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_7000_7&filter=topsellers&tags=19&infinite=1'
+
+    total_games = requests.get(url).json()['total_count']
+
+    for game in range(0, total_games, 50):
+        page_url = f'https://store.steampowered.com/search/results/?query&start={game}&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_7000_7&filter=topsellers&tags=19&infinite=1'
+        
+        game_data.append(extract_games(page_url))
+    print(game_data)
     
-
-
-data_extract = extract_games('https://store.steampowered.com/search/results/?query&start=0&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_7000_7&filter=topsellers&tags=19&infinite=1')
-
-print(data_extract[0])
+    game_df = pd.concat([pd.DataFrame(game )for game in game_data])
+   
+    game_df.to_csv('game_data.csv', index = False)
